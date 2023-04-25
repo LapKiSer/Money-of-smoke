5ea73e496626ee294b3d88e5dadde0638b3e4b33
LapKiSer <lapkovskiykirill@gmail.com>
2023-04-25 17:50:05 +0300

    Initial Committ

---
+++ Money of smoke/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money of smoke
+//
+//  Created by Кирилл Лапковский on 25.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
