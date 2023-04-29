da4929f29dd2890f58d61e54d56dd666d79e7d3e
Alex Zhulai <Lepiozdyx@gmail.com>
2023-04-29 19:34:46 -0300

    Only the main elements + code

---
+++ Money of smoke/CalculateViewController.swift
@@ -0,0 +1,53 @@
+//
+//  CalculateViewController.swift
+//  Money of smoke
+//
+//  Created by Alex on 29.04.2023.
+//
+
+import UIKit
+
+protocol SettingsViewControllerDelegate: AnyObject {
+    func didUpdateCalculator(_ calculator: Calculator)
+}
+
+final class CalculateViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet var moneyLabel: UILabel!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var lifeLabel: UILabel!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var calculator = Calculator.getValues()
+    
+    // MARK: - ViewlifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startButton.tintColor = .systemMint
+        updateLabels()
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let settingsVC = segue.destination as? SettingsViewController {
+            settingsVC.delegate = self
+            settingsVC.calculator = calculator
+        }
+    }
+    
+    // MARK: - PrivateMethods
+    private func updateLabels() {
+        moneyLabel.text = String(format: "%.1f", calculator.moneySaved)
+        timeLabel.text = String(format: "%d", calculator.daysWithoutSmoking)
+        lifeLabel.text = String(format: "%.1f", calculator.lifeSavedDays)
+    }
+}
+
+// MARK: - SettingsViewControllerDelegate
+extension CalculateViewController: SettingsViewControllerDelegate {
+    func didUpdateCalculator(_ calculator: Calculator) {
+        self.calculator = calculator
+        updateLabels()
+    }
+}
---
+++ Money of smoke/SettingsViewController.swift
@@ -0,0 +1,116 @@
+//
+//  SettingsViewController.swift
+//  Money of smoke
+//
+//  Created by Alex on 29.04.2023.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet var costTextField: UITextField!
+    @IBOutlet var quantityTextField: UITextField!
+    
+    @IBOutlet var firstDatePicker: UIDatePicker!
+    @IBOutlet var lastDatePicker: UIDatePicker!
+    
+    var calculator: Calculator!
+    unowned var delegate: SettingsViewControllerDelegate!
+    
+    // MARK: - ViewLifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        costTextField.delegate = self
+        quantityTextField.delegate = self
+        
+        costTextField.keyboardType = .decimalPad
+        quantityTextField.keyboardType = .numberPad
+        
+        lastDatePicker.minimumDate = firstDatePicker.date
+        
+        if calculator.isValuesSet {
+            costTextField.text = String(format: "%.2f", calculator.cigarettePackagePrice)
+            quantityTextField.text = String(format: "%d", calculator.cigarettesPerDay)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    // MARK: - IBActions
+    @IBAction func saveButtonPressed() {
+        guard let costString = costTextField.text, let cost = Double(costString), let quantityString = quantityTextField.text, let quantity = Int(quantityString) else {
+            getAlert(withTitle: "Упс!", andMessage: "Пожалуйста, введите данные..")
+            return
+        }
+        
+        let firstDate = firstDatePicker.date
+        let lastDate = lastDatePicker.date
+        
+        let updateCalculator = Calculator(cigarettePackagePrice: cost, cigarettesPerDay: quantity, firstDate: firstDate, lastDate: lastDate)
+        
+        delegate.didUpdateCalculator(updateCalculator)
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: - PrivateMethods
+    private func getAlert(
+        withTitle title: String,
+        andMessage message: String,
+        textField: UITextField? = nil
+    ) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
+            textField?.becomeFirstResponder()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension SettingsViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text, !text.isEmpty, let value = Double(text), (1...9999).contains(value) else {
+            getAlert(withTitle: "Ошибка", andMessage: "Попробуйте еще..", textField: textField)
+            textField.layer.borderColor = UIColor.red.cgColor
+            textField.layer.borderWidth = 1
+            return
+        }
+        
+        textField.layer.borderColor = UIColor.clear.cgColor
+        textField.layer.borderWidth = 0
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let keyboardToolbar = UIToolbar()
+        keyboardToolbar.sizeToFit()
+        textField.inputAccessoryView = keyboardToolbar
+        
+        let doneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: textField,
+            action: #selector(resignFirstResponder)
+        )
+        
+        let flexBarButton = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: nil,
+            action: nil
+        )
+        
+        keyboardToolbar.items = [flexBarButton, doneButton]
+    }
+    
+}
