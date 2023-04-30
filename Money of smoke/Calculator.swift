a4f19204f26afb7bdcb7e47f28e2ab14d15b1a5f
LapKiSer <lapkovskiykirill@gmail.com>
2023-04-30 10:55:55 +0300

    Add Alex Files to project

---
+++ Money of smoke/Calculator.swift
@@ -0,0 +1,47 @@
+//
+//  Calculator.swift
+//  Money of smoke
+//
+//  Created by Alex on 29.04.2023.
+//
+
+import Foundation
+
+struct Calculator {
+    let cigarettePackagePrice: Double
+    let cigarettesPerDay: Int
+    let firstDate: Date
+    let lastDate: Date
+    
+    var daysWithoutSmoking: Int {
+        if cigarettesPerPackage == 0 || cigarettesPerDay == 0 || firstDate == lastDate {
+            return 0
+        }
+        
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.day], from: firstDate, to: lastDate)
+        let days = components.day ?? 0
+        return days + 1
+    }
+    
+    var moneySaved: Double {
+        let pricePerCigarette = cigarettePackagePrice / cigarettesPerPackage
+        return pricePerCigarette * Double(cigarettesPerDay) * Double(daysWithoutSmoking)
+    }
+    
+    var lifeSavedDays: Double {
+        let minutesSavedPerCigarette = 11
+        let lifeSavedPerDay = Double(cigarettesPerDay * minutesSavedPerCigarette)
+        return lifeSavedPerDay * Double(daysWithoutSmoking) / (24 * 60)
+    }
+    
+    var isValuesSet: Bool {
+        return cigarettePackagePrice != 0 && cigarettesPerDay != 0
+    }
+    
+    private let cigarettesPerPackage = 20.0
+
+    static func getValues() -> Calculator {
+        Calculator(cigarettePackagePrice: 0, cigarettesPerDay: 0, firstDate: Date(), lastDate: Date())
+    }
+}
